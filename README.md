2e40d15797d07768fd47a6aa19eff5a2c609e033
goldinio1963 <ablermav@hotmail.com>
2020-05-09 11:41:41 -0500

    First version

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Bookmark with mongoose and development</h1>
+
+<p>This repository is for the node laboratory with mongoose and the development</p>
---
+++ public/js/index.js
@@ -0,0 +1,231 @@
+const API_TOKEN = '2abbf7c3-245b-404f-9473-ade729ed4653'
+
+function addBookmarkFetch(id, title, description, burl, rating) {
+    let url = '/bookmarks';
+    let data = {
+        id : id,
+        title : title,
+        description : description,
+        url : burl,
+        rating : Number(rating)
+    }
+    let settings = {
+        method : 'POST',
+        headers : {
+            Authorization : `Bearer ${API_TOKEN}`,
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify(data)
+    }
+    
+    let results = document.querySelector('.results');
+
+    fetch( url, settings )
+        .then( response => {
+            if( response.ok ){
+                return response.json();
+            }
+            throw new Error( response.statusText );
+        })
+
+        .then( responseJSON => {
+            fetchBookmarks();
+        })
+        .catch( err => {
+            results.innerHTML = `<div> ${err.message} </div>`;
+        });
+}
+
+function fetchBookmarks() {
+    let url = '/bookmarks';
+    let settings = {
+        method : 'GET',
+        headers : {
+            Authorization : `Bearer ${API_TOKEN}`
+        }
+    }
+    let results = document.querySelector( '.results' );
+    fetch( url, settings )
+        .then( response => {
+            if( response.ok ){
+                return response.json();
+            }
+            throw new Error( response.statusText );
+        })
+        .then( responseJSON => {
+            results.innerHTML = "";
+            for ( let i = 0; i < responseJSON.length; i ++ ){
+                results.innerHTML += `<div> ${responseJSON[i].name} </div>`;
+            }
+        })
+        .catch( err => {
+            results.innerHTML = `<div> ${err.message} </div>`;
+        });
+}
+
+function DeleteBookmarkFetch(id){
+    let url = '/bookmark/' + id;
+
+    let settings = {
+        method : 'DELETE',
+        headers : {
+            Authorization : `Bearer ${API_TOKEN}`
+        }
+    }
+
+    let results = document.querySelector('.results');
+
+    fetch( url, settings )
+        .then( response => {
+            if( response.ok ){
+                return response.json();
+            }
+            throw new Error( response.statusText );
+        })
+
+        .then( responseJSON => {
+            fetchBookmarks();
+        })
+        .catch( err => {
+            results.innerHTML = `<div> ${err.message} </div>`;
+        });
+}
+
+function UpdateBookmarkFetch(bid,title,description,burl,rating) {
+    let url = '/bookmark/' + id;
+
+    let data = {
+        title : title,
+        description : description,
+        url : burl,
+        rating : Number(rating)
+    }
+    let settings = {
+        method : 'PATCH',
+        headers : {
+            Authorization : `Bearer ${API_TOKEN}`,
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify(data)
+    }
+    
+    let results = document.querySelector('.results');
+
+    fetch( url, settings )
+        .then( response => {
+            if( response.ok ){
+                return response.json();
+            }
+            throw new Error( response.statusText );
+        })
+
+        .then( responseJSON => {
+            fetchBookmarks();
+        })
+        .catch( err => {
+            results.innerHTML = `<div> ${err.message} </div>`;
+        }); 
+}
+
+function SearchBookmarkFetch(serchTitle){
+    let url = '/bookmark';
+    
+    let data = {
+        title : serchTitle
+    };
+
+    let settings = {
+        method : 'GET',
+        headers : {
+            Authorization : `Bearer ${API_TOKEN}`,
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify(data)
+    }
+    let results = document.querySelector( '.results' );
+    fetch( url, settings )
+        .then( response => {
+            if( response.ok ){
+                return response.json();
+            }
+            throw new Error( response.statusText );
+        })
+        .then( responseJSON => {
+            results.innerHTML = "";
+            for ( let i = 0; i < responseJSON.length; i ++ ){
+                results.innerHTML += `<div> ${responseJSON[i].name} </div>`;
+            }
+        })
+        .catch( err => {
+            results.innerHTML = `<div> ${err.message} </div>`;
+        });
+}
+
+//Watch
+function watchBookmarksform() {
+    let submitbotton = document.querySelector('.Bookmark-form')
+
+    submitbotton.addEventListener('submit', (event) => {
+        event.preventDefault();
+        fetchBookmarks();
+    });
+}
+
+function watchAddBookmarkForm() {
+    let submitbotton = document.querySelector('.add-bookmark-form')
+
+    submitbotton.addEventListener( 'submit' , ( event ) => {
+        event.preventDefault();
+        let id = document.getElementById( 'bookmarkID' ).value;
+        let title = document.getElementById( 'bookmarkTitle' ).value;
+        let description = document.getElementById('bookmarkDescription').value;
+        let url = document.getElementById('bookmarkURL').value;
+        let rating = document.getElementById('bookmarkRating').value;
+        addBookmarkFetch( id, title, description, url, rating );
+    })
+}
+
+function watchDeleteOneForm(){
+    let submitbotton = document.querySelector('.delete-bookmark-form')
+
+    submitbotton.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let id = document.getElementById('deletebookmarkID').value;
+        DeleteBookmarkFetch(id);
+    })
+}
+
+function watchUpdateForm(){
+    let submitbotton = document.querySelector('.patch-bookmark-form')
+
+    submitbotton.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let bid = document.getElementById( 'bodyPatchBookmarkID' ).value;
+        let title = document.getElementById( 'patchbookmarkTitle' ).value;
+        let description = document.getElementById('patchbookmarkDescription').value;
+        let url = document.getElementById('patchbookmarkURL').value;
+        let rating = document.getElementById('patchbookmarkRating').value;
+        UpdateBookmarkFetch(bid,title,description,url,rating)
+    })
+}
+
+function watchOneBookmarkForm(){
+    let submitbotton = document.querySelector('.getone-bookmark-form')
+
+    submitbotton.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let serchTitle = document.getElementById('GetTitle')
+        SearchBookmarkFetch(serchTitle);
+    })
+}
+
+function init(){
+    
+    watchBookmarksform();
+    watchAddBookmarkForm();
+    watchDeleteOneForm();
+    watchUpdateForm();
+    watchOneBookmarkForm();
+}
+
+init();
